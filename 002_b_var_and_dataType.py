9aafdbfa41e95b1b369194189f6ff4ae70b8e555
javier-cantu <pacocantu87@gmail.com>
2020-01-14 01:01:44 -0600

    Update 002_b_var_and_dataType.py

--- 002_b_var_and_dataType.py
+++ 002_b_var_and_dataType.py
@@ -7,4 +7,4 @@ character_age = 32
 print("There once was a man named " + character_name + ", ")
 print("he was " + str(character_age) + " years old. ")
 print("He really liked the name " + character_name + ", ")
-print("but didn't like being 70")
+print("but didn't like being " + str(character_age))
