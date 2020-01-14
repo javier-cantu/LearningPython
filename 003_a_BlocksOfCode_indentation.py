04e56a76e142352f3785a495d48e74e2714fb41b
javier-cantu <pacocantu87@gmail.com>
2020-01-14 01:17:31 -0600

    Create 003_a_BlocksOfCode_indentation.py

---
+++ 003_a_BlocksOfCode_indentation.py
@@ -0,0 +1,11 @@
+# Blocks of code 
+# Blocks are easier to understand by looking at some indented code
+
+name = 'Mary'
+password = 'swordfish'
+if name == 'Mary':
+    print('Hello, Mary')
+    if password == 'swordfish':
+        print('Access granted.')
+    else:
+        print('Wrong password.')
