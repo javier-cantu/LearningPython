2dbc9a85a28f9b6f9d982731b4e820a32f51b477
Javier <pacocantu87@gmail.com>
2020-01-30 01:10:04 -0600

    Create 010_e_IntroListFunctionsClear.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/010_e_IntroListFunctionsClear.py
@@ -0,0 +1,16 @@
+# 010_e_IntroListFunctionsClear.py
+
+# Una lista es una estructura para guardar informacion
+# Puede almacenar cualquier tipo de variable, int, float, string, bool
+lista_numeros = [1, 2, 5, 12, 65, 23, 31]
+lista_humanos = ["Yamsha", "Goku", "Jesus", "Bush"]
+
+# Se imprime la lista "lista_humanos"
+print(lista_humanos)
+
+# Existen muchas funciones para trabajar con listas en python
+# Una de ellas es .clear() que se usa para resetear y limpiar una lista
+lista_humanos.clear()
+
+# Se vuelve a imprimir la "lista_humanos" y esta vacia
+print(lista_humanos) # []
