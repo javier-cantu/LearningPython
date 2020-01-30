2a23d092ecd7c42730c1a489424d1eef548e0572
Javier <pacocantu87@gmail.com>
2020-01-30 01:20:16 -0600

    Create 010_h_IntroListFunctionsCount.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/010_h_IntroListFunctionsCount.py
@@ -0,0 +1,13 @@
+# 010_h_IntroListFunctionsCount.py
+
+# Una lista es una estructura para guardar informacion
+# Puede almacenar cualquier tipo de variable, int, float, string, bool
+lista_numeros = [1, 2, 5, 12, 65, 23, 31]
+lista_humanos = ["Yamsha", "Goku", "Goku", "Goku", "Jesus", "Bush"]
+
+# Se imprime la lista "lista_humanos"
+print(lista_humanos)
+
+# Existen muchas funciones para trabajar con listas en python
+# Una de ellas es .count() que se usa para contar cuantas veces aparece un elemento especifico en una lista.
+print(lista_humanos.count("Goku"))  # 3
