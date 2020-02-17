0b47e0a8b4b0b11ddef4485fce5e6c31bfb52692
Javier <pacocantu87@gmail.com>
2020-02-17 10:01:46 -0800

    Create 012_b_FunctionsParameterIntro.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/012_b_FunctionsParameterIntro.py
@@ -0,0 +1,12 @@
+# 012_b_FunctionsParameterIntro.py
+
+# A una funcion se le puede dar información que se llama "parameters"
+
+# El parametro se nombra dentro del parentesis al definir la funcion
+# El parametro es como una variable que se usa dentro de la funcion
+def print_name(name):
+    print("Hello " + name)
+
+# Asi se llama la funcion enviando un parametro
+print_name("Brian") # Hello Brian
+print_name("Arnold") # Hello Monthy
