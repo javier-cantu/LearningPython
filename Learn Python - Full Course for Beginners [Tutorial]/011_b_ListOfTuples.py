50385b6639dbaac4fd518599b2286631b166a33b
Javier <pacocantu87@gmail.com>
2020-02-17 08:27:14 -0800

    Create 011_b_ListOfTuples.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/011_b_ListOfTuples.py
@@ -0,0 +1,14 @@
+# 011_b_ListOfTuples.py
+
+# Un Tuple es una estructura para guardar informacion
+# Puede almacenar cualquier tipo de variable, int, float, string, bool
+# Pero es "inmutable" no se puede editar como una lista, ni agregar, ni reemplazar nuevos elementos.
+# Se pone entre ()
+
+# Asi se crea una lista de tuples
+coordenadas1 = [(3,5), (6,2), (20,5)]
+
+# Para accesar a cada elemento de la lista se usa su index number entre []
+print(coordenadas1[0]) # (3,5)
+print(coordenadas1[1]) # (6,2)
+print(coordenadas1[2]) # (20,5)
