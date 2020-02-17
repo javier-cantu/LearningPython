2f7034d6e2aae33302759594883d3908e19f44c4
Javier <pacocantu87@gmail.com>
2020-02-17 12:34:36 -0800

    Create 013_b_ReturnStatementIntro.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/013_b_ReturnStatementIntro.py
@@ -0,0 +1,10 @@
+# 013_b_ReturnStatementIntro.py
+
+# El Return Statement, es informacion que genera y regresa una funcion al ejecutarse.
+def cube(x):
+    return x*x*x
+
+# Asignamos el return value de cube(4) a una variable llamada result
+result = cube(4)
+# Imprimimos el valor de la nueva variable
+print(result) # 64
