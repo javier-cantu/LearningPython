0ff89d2eaa53af621c7d704ad4709d190a4a2deb
Javier <pacocantu87@gmail.com>
2020-02-17 06:38:04 -0800

    Create  011_a_TuplesIntro.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/011_a_TuplesIntro.py
@@ -0,0 +1,13 @@
+# 011_a_TuplesIntro.py
+
+# Un Tuple es una estructura para guardar informacion
+# Puede almacenar cualquier tipo de variable, int, float, string, bool
+# Pero es "inmutable" no se puede editar como una lista, ni agregar, ni reemplazar nuevos elementos.
+# Se pone entre ()
+
+# Asi se crea un tuple con 2 valores.
+coordenadas1 = (3,5)
+
+# PAra accesar a cada elemento del tuple se usa su index number entre []
+print(coordenadas1[0]) # 3
+print(coordenadas1[1]) # 5
