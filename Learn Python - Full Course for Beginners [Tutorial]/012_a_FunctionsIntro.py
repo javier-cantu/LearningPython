fb76943f731ca1dca8b3a55c3c426b65a0a19582
Javier <pacocantu87@gmail.com>
2020-02-17 09:53:38 -0800

    Create 012_a_FunctionsIntro.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/012_a_FunctionsIntro.py
@@ -0,0 +1,11 @@
+# 012_a_FunctionsIntro.py
+
+# Una Function es un pedazo de codigo que hace una tarea específica
+# Se manda llamar para evitar reescribirla cada vez que se necsite.
+
+# Asi se define o crea una función
+def helloWorld_Function():
+    print("Hello World") # El codigo dentro de la funcion va indentado
+
+# Asi se llama la funcion
+helloWorld_Function()
