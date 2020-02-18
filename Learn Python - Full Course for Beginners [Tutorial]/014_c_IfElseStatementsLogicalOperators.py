9a505d40756521e2d17c6e5463a47d08bcd2c572
Javier <pacocantu87@gmail.com>
2020-02-18 15:34:21 -0800

    Create 014_c_IfElseStatementsLogicalOperators.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/014_c_IfElseStatementsLogicalOperators.py
@@ -0,0 +1,12 @@
+# 014_c_IfElseStatementsLogicalOperators.py
+
+# Primero declaramos 2 variables boolean
+# Experimentar con cambiar sus valores a False para ver el resultado
+a = True
+b = True
+
+# Luego podemos utilizar comparadores logicos para determinar que se ejecuta al comparar las variables.
+if a or b:
+    print("a o b son True")
+else:
+    print("a y b son False")
