1c2677da84a2cf061da6053e29df42c20c484e33
Javier <pacocantu87@gmail.com>
2020-02-18 15:38:34 -0800

    Update 014_c_IfElseStatementsLogicalOperators.py

--- Learn Python - Full Course for Beginners [Tutorial]/014_c_IfElseStatementsLogicalOperators.py
+++ Learn Python - Full Course for Beginners [Tutorial]/014_c_IfElseStatementsLogicalOperators.py
@@ -1,12 +1,20 @@
 # 014_c_IfElseStatementsLogicalOperators.py
 
+# Este script se debe ejecutar varias veces cambiando los valore de las variables
 # Primero declaramos 2 variables boolean
 # Experimentar con cambiar sus valores a False para ver el resultado
 a = True
 b = True
 
 # Luego podemos utilizar comparadores logicos para determinar que se ejecuta al comparar las variables.
+# OR
 if a or b:
     print("a o b son True")
 else:
     print("a y b son False")
+
+# AND
+if a and b:
+    print("a y b son Iguales")
+else:
+    print("a o b son Diferentes")
