4c7283e5e1d60875698a5b57da6b87c1fc8b66ce
Javier <pacocantu87@gmail.com>
2020-02-18 15:13:27 -0800

    Create 014_a_IfStatementsIntro.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/014_a_IfStatementsIntro.py
@@ -0,0 +1,13 @@
+# 014_a_IfStatementsIntro.py
+
+# El If statement se utiliza para ejecutar una parte del código cuando una condición se cumpla
+# Permite al programa tomar desiciones dependiendo del input o de un cálculo.
+
+# Declarar una boolean variable
+is_daytime = True
+
+# Asi se crea un if statement empezando por If
+# Luego la condición
+# En este caso es una variable boolean que es True
+if is_daytime:
+    print("Is daytime")
