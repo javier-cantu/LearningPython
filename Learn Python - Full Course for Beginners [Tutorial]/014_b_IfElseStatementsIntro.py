9d6d9400935d58117b200b783a89362e48914709
Javier <pacocantu87@gmail.com>
2020-02-18 15:16:22 -0800

    Create 014_b_IfElseStatementsIntro.py

---
+++ Learn Python - Full Course for Beginners [Tutorial]/014_b_IfElseStatementsIntro.py
@@ -0,0 +1,15 @@
+# 014_b_IfElseStatementsIntro.py
+
+# Else permite que se ejecute otra parte del codigo si no se cumple la condicion del if
+
+# Declarar una boolean variable
+is_daytime = False # Ahora es false para que no se cumple el if
+
+# Asi se crea un if statement empezando por If
+# Luego la condición
+# En este caso si no ese cumple la condicion del if
+# Se ejecuta el bloque de else
+if is_daytime:
+    print("Is daytime")
+else: # else con :
+    print("Is nighttime!")
